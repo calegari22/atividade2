f2be7e44c533e0243aeb857dbcfdc6927cc1fbbe
calegari22 <gustavo.calegari22@icloud.com>
2025-11-19 20:05:04 -0300

    server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json({ mensagem: 'Olá, mundo!' });
+  res.json({ mensagem: 'Atividade 2 teste! A aplicação está funcionando!' });
 });
 
 const PORT = process.env.PORT || 3000;
